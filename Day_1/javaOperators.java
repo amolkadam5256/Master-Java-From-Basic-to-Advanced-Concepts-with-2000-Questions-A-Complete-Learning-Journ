2e0e5fe489818c94e7e96715d95299a0987816b6
amolkadam5256 <amolkadam1274@gmail.com>
2025-01-14 12:19:24 +0530

    Add practice file to clear concept Java objects

--- Day_1/javaOperators.java
+++ Day_1/javaOperators.java
@@ -1,34 +1,34 @@
-public class javaOperators {
-    public static void main(String[] args){
-        System.out.println("Java Operators\n");
-        System.out.println("Arithmetic operators\n" +
-                "Assignment operators\n" +
-                        "Comparison operators\n" +
-                        "Logical operators\n" +
-                        "Bitwise operators\n");
-        System.out.println("Arithmetic operators");
-        System.out.println("Operator\tName" + "\n" +
-                "+\tAddition" +"\n" +
-                "-\tSubtraction" +"\n" +
-                "*\tMultiplication" +"\n" +
-                "/\tDivision" +"\n" +
-                "%\tModulus" +"\n" +
-                "++\tIncrement" +"\n" +
-                "--\tDecrement~\n");
+        public class javaOperators {
+        public static void main(String[] args){
+                System.out.println("Java Operators\n");
+                System.out.println("Arithmetic operators\n" +
+                        "Assignment operators\n" +
+                                "Comparison operators\n" +
+                                "Logical operators\n" +
+                                "Bitwise operators\n");
+                System.out.println("Arithmetic operators");
+                System.out.println("Operator\tName" + "\n" +
+                        "+\tAddition" +"\n" +
+                        "-\tSubtraction" +"\n" +
+                        "*\tMultiplication" +"\n" +
+                        "/\tDivision" +"\n" +
+                        "%\tModulus" +"\n" +
+                        "++\tIncrement" +"\n" +
+                        "--\tDecrement~\n");
 
-        System.out.println("Java Assignment Operators\n");
-        System.out.println("Operator\tName" + "\n" +
-                "=\tAddition" +"\n" +
-                "+=\taddition assignment (+=) x = x + 3" +"\n" +
-                "-=\tSubtraction assignment (-=) x = x - 3" +"\n" +
-                "*=\tMultiplication assignment (*=) x = x * 3" +"\n" +
-                "/=\tDivision assignment (/=) x = x / 3" +"\n" +
-                "%=\tModules assignment (%=) x = x % 3" +"\n" +
-                "&=\tBitwise AND assignment operator.  assignment (&=) x = x & 3" +"\n" +
-                "|=\tbitwise-OR-assignment operator  assignment (|=) x = x | 3\"" +"\n" +
-                "^=\tbitwise exclusive OR and assignment operator.  assignment (^=) x = x ^ 3\"" +"\n" +
-                ">>=\tright shift operator  assignment (>>=) x = x >> 3\"" +"\n" +
-                "<<=\tleft shift operator  assignment (>>=) x = x << 3\"~\n");
-    }
+                System.out.println("Java Assignment Operators\n");
+                System.out.println("Operator\tName" + "\n" +
+                        "=\tAddition" +"\n" +
+                        "+=\taddition assignment (+=) x = x + 3" +"\n" +
+                        "-=\tSubtraction assignment (-=) x = x - 3" +"\n" +
+                        "*=\tMultiplication assignment (*=) x = x * 3" +"\n" +
+                        "/=\tDivision assignment (/=) x = x / 3" +"\n" +
+                        "%=\tModules assignment (%=) x = x % 3" +"\n" +
+                        "&=\tBitwise AND assignment operator.  assignment (&=) x = x & 3" +"\n" +
+                        "|=\tbitwise-OR-assignment operator  assignment (|=) x = x | 3\"" +"\n" +
+                        "^=\tbitwise exclusive OR and assignment operator.  assignment (^=) x = x ^ 3\"" +"\n" +
+                        ">>=\tright shift operator  assignment (>>=) x = x >> 3\"" +"\n" +
+                        "<<=\tleft shift operator  assignment (>>=) x = x << 3\"~\n");
+        }
 
-}
+        }
---
+++ Day_5_Pratic/Pratice.java
@@ -0,0 +1,5 @@
+package Day_5_Pratic;
+
+public class Pratice {
+    
+}
