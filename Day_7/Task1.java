fc8e662f2068c718455b797f57f32ea1af900c15
amolkadam5256 <amolkadam1274@gmail.com>
2025-01-28 13:37:15 +0530

    added day 7 and solve 1 hackerrank task

---
+++ Day_7/Task1.java
@@ -0,0 +1,22 @@
+// Task
+
+// In this challenge, you must read integers from stdin and then print them to
+// stdout. Each integer must be printed on a new line. To make the problem a
+// little easier, a portion of the code is provided for you in the editor below.
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Read integers until there is no more input
+        while (scanner.hasNext()) {
+            int myInt = scanner.nextInt();
+            System.out.println(myInt);
+        }
+        scanner.close();
+
+    }
+}
