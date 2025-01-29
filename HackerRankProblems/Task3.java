b03cc3cb91e57a6b9cdfff21de15114fef3ce65e
amolkadam5256 <amolkadam1274@gmail.com>
2025-01-29 11:18:05 +0530

    solve task 3 in HackerRank problem to Input Format

---
+++ HackerRankProblems/Task3.java
@@ -0,0 +1,21 @@
+package HackerRankProblems;
+
+import java.util.Scanner;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        double d = sc.nextDouble();
+
+        sc.nextLine();
+
+        String str = sc.nextLine();
+
+        System.out.println("String: " + str);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + n);
+    }
+}
